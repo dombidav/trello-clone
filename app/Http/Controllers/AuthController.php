2833a77ada49f76e467fbc33a57267b2e35f7387
Nagy Ákos <nagyakos19971127@gmail.com>
2022-02-16 14:07:31 +0100

    Added AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Http\Controllers;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Utils\ResponseCodes;
+use http\Env\Response;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+
+    public function login(LoginRequest $request){
+        $token = auth()->attempt($request->validated());
+        if(!$token) {
+            return response()->json(['error' => 'Unauthorized'], ResponseCodes::UNAUTHORIZED);
+        }
+        return $this->createNewToken($token);
+    }
+
+
+    public function register(StoreUserRequest $request, UserController $userController) {
+        return $userController->store($request);
+    }
+
+
+    public function logout() {
+        auth()->logout();
+        return response()->json(['message' => 'User successfully signed out']);
+    }
+
+
+    public function refresh() {
+        return $this->createNewToken(auth()->refresh());
+    }
+
+
+    public function userProfile() {
+        return response()->json(auth()->user());
+    }
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ]);
+    }
+}
