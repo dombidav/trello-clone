7b4084f08b2bb239e559f775806ab373870bc4a3
Dombi Tibor <dombidav@gmail.com>
2022-02-16 09:23:26 +0100

    Added Password rule

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use App\Models\User;
+use App\Rules\IsValidPassword;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -21,7 +22,7 @@ class UpdateUserRequest extends FormRequest
         return [
             'username' => ['max:32', Rule::unique('users', 'username')->ignore($this->user->id)],
             'email' => ['email', Rule::unique('users', 'email')->ignore($this->user->id)],
-            'password' => ['required', 'min:8', 'confirmed', /* 'password_format' */ ], // TODO: Add password validation rule
+            'password' => ['required', 'min:8', 'confirmed', new IsValidPassword ],
             'current_password' => ['required', 'current_password:api'], // TODO: Only allow password change if the current user is the same as the user being updated
         ];
     }
