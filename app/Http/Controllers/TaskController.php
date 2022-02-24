fce41aad14d9eee322abb8de5756e1da21c46789
Dombi Tibor <dombidav@gmail.com>
2022-02-24 13:14:45 +0100

    Fixed merge

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,7 +9,6 @@ use App\Utils\ResponseCodes;
 
 class TaskController extends Controller
 {
-<<<<<<< HEAD
     /**
      * Display a listing of the resource.
      *
@@ -19,28 +18,16 @@ class TaskController extends Controller
     {
         return Task::all();
     }
-=======
->>>>>>> master
-
-    public function index()
-    {
-        return Task::all();
-    }
 
     function store(StoreTaskRequest $request)
     {
         $validated = $request->validated();
-<<<<<<< HEAD
 
        /* $validated[''] = ($request,['' => 'required|max:255',]); */
 
         $task = Task::create($validated);
 
         return response($task, 201);
-=======
-        $task = Task::create($validated);
-        return response($task, ResponseCodes::CREATED);
->>>>>>> master
     }
 
     public function show(Task $task)
@@ -54,22 +41,9 @@ class TaskController extends Controller
         return $task;
     }
 
-<<<<<<< HEAD
-     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Task $task)
     {
         $task->delete();
         return response()->json(null, 204);
-=======
-    public function destroy(Task $task)
-    {
-        $task->delete();
-        return response()->json(null, ResponseCodes::NO_CONTENT);
->>>>>>> master
     }
 }
